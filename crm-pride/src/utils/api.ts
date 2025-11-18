9dff7863829ab8d3624ef145e68e1e732a11049f
klu4 <test@MB426657.local>
2025-11-18 17:05:00 +0300

    auth fix

--- crm-pride/src/utils/api.ts
+++ crm-pride/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://preeminent-karisa-nonministerially.ngrok-free.dev/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || '77.95.201.169/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
