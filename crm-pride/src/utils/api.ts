5f38ee7a6d3a8eb55648fa5bcdab0e3c694970d9
klu4 <test@MB426657.local>
2025-11-25 21:23:08 +0300

    admin features

--- crm-pride/src/utils/api.ts
+++ crm-pride/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://matching-wal-started-humanities.trycloudflare.com/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://marked-dependence-interventions-shelf.trycloudflare.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
