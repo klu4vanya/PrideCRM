9e996ea2d85f4db33525fea6731cc17d7a58175d
klu4 <test@MB426657.local>
2025-12-04 15:46:31 +0300

    design changes

--- crm-pride/src/utils/api.ts
+++ crm-pride/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://satisfied-ellen-scripting-cayman.trycloudflare.com/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || ' https://postal-accessories-occurred-hey.trycloudflare.com/api';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
