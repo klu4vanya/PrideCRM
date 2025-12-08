1550a3d34004ca4c8311337c8ea3175dc0fd3986
klu4 <test@MB426657.local>
2025-12-08 23:50:00 +0300

    backend_hosting

--- crm-pride/src/utils/api.ts
+++ crm-pride/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://almost-employer-responsible-hang.trycloudflare.com/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || '5.129.246.53';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
