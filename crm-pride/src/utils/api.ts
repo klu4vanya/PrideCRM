444f0a5a2077c149b7e3046c7e954eea488be031
klu4 <test@MB426657.local>
2025-12-16 23:51:45 +0300

    backend_hosting

--- crm-pride/src/utils/api.ts
+++ crm-pride/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || '5.129.246.53:8000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'api.pridepokerclub.ru';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
